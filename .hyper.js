01dffc349e1c4a61947b6495ad01ddef879d8a2f
Robbie McClellan <rlm8291@gmail.com>
2019-05-11 20:56:56 -0400

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -115,8 +115,8 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-dracula", "hyper-quit", "gitrocket"],
-
+  plugins: ["hyperterm-panda", "hyper-quit", "gitrocket"],
+	
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
